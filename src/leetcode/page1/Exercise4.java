b25757b343c00287763d5ceb9cecb1b3bc2a57c7
qqins <1007797840@qq.com>
2018-05-21 20:12:04 +0800

    test

--- src/leetcode/page1/Exercise4.java
+++ src/leetcode/page1/Exercise4.java
@@ -24,6 +24,7 @@ import java.util.List;
 public class Exercise4 {
     /**
      * 将两个数组中较小的存在链表中，由于要取中位数，所以只需循环一半
+     *
      * @param nums1
      * @param nums2
      * @return
@@ -59,6 +60,7 @@ public class Exercise4 {
     /**
      * https://www.jianshu.com/p/7c990b12a82a
      * 参考答案，复杂度为O(log(m+n))
+     *
      * @param A
      * @param B
      * @return
@@ -68,38 +70,51 @@ public class Exercise4 {
         int n = B.length;
         // to ensure m<=n
         if (m > n) {
-            int[] temp = A; A = B; B = temp;
-            int tmp = m; m = n; n = tmp;
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
         }
         int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
             int j = halfLen - i;
-            if (i < iMax && B[j-1] > A[i]){
+            if (i < iMax && B[j - 1] > A[i]) {
                 // i is too small
                 iMin = iMin + 1;
-            }
-            else if (i > iMin && A[i-1] > B[j]) {
+            } else if (i > iMin && A[i - 1] > B[j]) {
                 // i is too big
                 iMax = iMax - 1;
-            }
-            else { // i is perfect
+            } else { // i is perfect
                 int maxLeft = 0;
-                if (i == 0) { maxLeft = B[j-1]; }
-                else if (j == 0) { maxLeft = A[i-1]; }
-                else { maxLeft = Math.max(A[i-1], B[j-1]); }
-                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
 
                 int minRight = 0;
-                if (i == m) { minRight = B[j]; }
-                else if (j == n) { minRight = A[i]; }
-                else { minRight = Math.min(B[j], A[i]); }
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
 
                 return (maxLeft + minRight) / 2.0;
             }
         }
         return 0.0;
     }
+
     public static void main(String[] args) {
         int[] nums1 = {1, 2};
         int[] nums2 = {3, 4};
