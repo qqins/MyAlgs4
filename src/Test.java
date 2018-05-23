e30fd8c4b59ae0acd1b523e4d30f60e664d392c9
qqins <1007797840@qq.com>
2018-05-23 20:50:16 +0800

    test

--- src/Test.java
+++ src/Test.java
@@ -1,22 +1,15 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Test {
     private static final Logger logger = LoggerFactory.getLogger(Test.class.getName());
 
     public static void main(String[] args) {
-        String string = "abcdef";
-        boolean flag = true;
-        char[] chars = string.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            int x = string.indexOf(chars[i]);
-            if (x != i) {
-                flag = false;
-                break;
-            }
-        }
-        System.out.println(flag);
-        System.out.println("".length());
+        Map<Integer, Integer> map = new HashMap<>();
+        System.out.println(map.get(1));
     }
 }
---
+++ src/leetcode/page1/Exercise5.java
@@ -0,0 +1,59 @@
+package leetcode.page1;
+
+import java.util.Objects;
+
+/**
+ * @author: Hello World
+ * @date: 2018/5/23 18:43
+ * <p>
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
+ * <p>
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba"也是一个有效答案。
+ * <p>
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
+public class Exercise5 {
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            //回文数为奇数时
+            int len1 = expandAroundCenter(s, i, i);
+            //回文数为偶数时
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            /**
+             * end-start表示上一个回文数的长度
+             */
+            if (len > end - start) {
+                /**
+                 * len-1是要考虑到当回文数长度为偶数
+                 * 当回文数为奇数时，len/2与（len-1）/2结果一样
+                 */
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int l = left;
+        int r = right;
+        while ((l >= 0 && r < s.length()) && (s.charAt(l) == s.charAt(r))) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+
+    public static void main(String[] args) {
+        Exercise5 ex = new Exercise5();
+        System.out.println(ex.longestPalindrome("babad"));
+    }
+}
