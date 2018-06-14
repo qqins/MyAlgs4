92a2ba475c19db2e5fecfb52206277ec2f2a484f
qqins <1007797840@qq.com>
2018-06-14 21:15:29 +0800

    test

--- src/algorithms4/chapter2/priorityqueue/MaxPQ.java
+++ src/algorithms4/chapter2/priorityqueue/MaxPQ.java
@@ -1,5 +1,6 @@
 package algorithms4.chapter2.priorityqueue;
 
+import java.util.NoSuchElementException;
 import java.util.Random;
 
 /**
@@ -20,30 +21,38 @@ public class MaxPQ<Key extends Comparable<Key>> {
 
     private void resize(int max) {
         Key[] temp = (Key[]) new Comparable[max];
-        for (int i = 0; i < N + 1; i++) {
+        for (int i = 0; i < N; i++) {
             temp[i] = pq[i];
         }
         pq = temp;
     }
 
     public void insert(Key v) {
-        if (N + 1 == pq.length) {
+        if (N == pq.length) {
             resize(2 * pq.length);
         }
-        pq[++N] = v;
-        swim(N);
+        pq[N] = v;
+        swim(++N);
     }
 
     public Key delMax() {
-        Key max = pq[1];
+        if (N == 0) {
+            throw new NoSuchElementException();
+        }
+        Key max = pq[0];
         /**
-         * 之所以不用exch(1,N); pq[N]==null;
+         * 在书上的写法中：之所以不用exch(1,N); pq[N]==null;
          * 交换的是1与N，然后将N即总数减1
          * 再将之前N指向的位置置为空
          * 或者这样写：exch(1,N); pq[N]=null; N--;
+         *
+         * 以下的写法是为了利用0下标，其中N始终指向一个空位置，即N比实际
+         * 长度大1，这也是为啥exch和less中i和j都要减1
+         *
+         * 以下的exch实际交换的是首尾元素
          */
-        exch(1, N--);
-        pq[N + 1] = null;
+        exch(1, N);
+        pq[--N] = null;
         sink(1);
         if (N > 0 && N == pq.length / 4) {
             resize(pq.length / 2);
@@ -52,13 +61,13 @@ public class MaxPQ<Key extends Comparable<Key>> {
     }
 
     private boolean less(int i, int j) {
-        return pq[i].compareTo(pq[j]) < 0;
+        return pq[i - 1].compareTo(pq[j - 1]) < 0;
     }
 
     private void exch(int i, int j) {
-        Key t = pq[i];
-        pq[i] = pq[j];
-        pq[j] = t;
+        Key t = pq[i - 1];
+        pq[i - 1] = pq[j - 1];
+        pq[j - 1] = t;
     }
 
     private void swim(int k) {
@@ -86,7 +95,7 @@ public class MaxPQ<Key extends Comparable<Key>> {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
-        for (int i = 0; i < N + 1; i++) {
+        for (int i = 0; i < N; i++) {
             sb.append(pq[i] + ", ");
         }
         sb.append("]");
@@ -96,7 +105,7 @@ public class MaxPQ<Key extends Comparable<Key>> {
     public static void main(String[] args) {
         MaxPQ<Integer> maxPQ = new MaxPQ();
         Random randomInt = new Random();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             maxPQ.insert(randomInt.nextInt(100));
         }
         System.out.println(maxPQ.toString());
---
+++ src/algorithms4/chapter2/sort/Heap.java
@@ -0,0 +1,61 @@
+package algorithms4.chapter2.sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @author: Hello World
+ * @date: 2018/6/14 19:27
+ */
+public class Heap {
+    public void sort(Comparable[] pq) {
+        int n = pq.length;
+        for (int k = n / 2; k >= 1; k--) {
+            sink(pq, k, n);
+        }
+        while (n > 1) {
+            exch(pq, 1, n--);
+            sink(pq, 1, n);
+        }
+    }
+
+    private void sink(Comparable[] pq, int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(pq, j, j + 1)) {
+                j++;
+            }
+            if (!less(pq, k, j)) {
+                break;
+            }
+            exch(pq, k, j);
+            k = j;
+        }
+    }
+
+    /**
+     * 为了便于分析，设定数组的下标从1开始
+     * 所以以下的i和j都要减1
+     */
+    private boolean less(Comparable[] pq, int i, int j) {
+        return pq[i - 1].compareTo(pq[j - 1]) < 0;
+    }
+
+    private void exch(Comparable[] pq, int i, int j) {
+        Comparable temp = pq[i - 1];
+        pq[i - 1] = pq[j - 1];
+        pq[j - 1] = temp;
+    }
+
+    public static void main(String[] args) {
+        Heap heap = new Heap();
+        Random randomInt = new Random();
+        Integer[] a = new Integer[20];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = randomInt.nextInt(100);
+        }
+        System.out.println(Arrays.toString(a));
+        heap.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
