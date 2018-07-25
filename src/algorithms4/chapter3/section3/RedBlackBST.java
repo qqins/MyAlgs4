7198b4fedcd48faf7b3acb290f759c4e252729fc
qqins <1007797840@qq.com>
2018-07-25 22:20:17 +0800

    test

--- src/algorithms4/chapter3/section3/RedBlackBST.java
+++ src/algorithms4/chapter3/section3/RedBlackBST.java
@@ -1,7 +1,11 @@
 package algorithms4.chapter3.section3;
 
 import com.sun.org.apache.regexp.internal.RE;
-import sun.misc.Cache;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
  * @author: Hello World
@@ -132,7 +136,7 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         if (isRed(h.right) && !isRed(h.left)) {
             h = rotateLeft(h);
         }
-        if (isRed(h.left) && !isRed(h.left.left)) {
+        if (isRed(h.left) && isRed(h.left.left)) {
             h = rotateRight(h);
         }
         if (isRed(h.left) && isRed(h.right)) {
@@ -140,5 +144,222 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         }
         h.size = 1 + size(h.right) + size(h.left);
         return h;
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("BST underflow");
+        }
+        if (!isRed(root.left) && !isRed(root.right)) {
+            root.color = RED;
+        }
+        root = deleteMin(root);
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
+
+    private Node deleteMin(Node h) {
+        if (h.left == null) {
+            return null;
+        }
+        if (!isRed(h.left) && !isRed(h.left.left)) {
+            h = moveRedLeft(h);
+        }
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private Node moveRedLeft(Node h) {
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    private Node balance(Node h) {
+        if (isRed(h.right)) {
+            h = rotateLeft(h);
+        }
+        if (isRed(h.left) && isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColors(h);
+        }
+        h.size = 1 + size(h.left) + size(h.right);
+        return h;
+    }
+
+    public void deleteMax() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("BST underflow");
+        }
+        if (!isRed(root.left) && !isRed(root.right)) {
+            root.color = RED;
+        }
+        root = deleteMax(root);
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
+
+    private Node deleteMax(Node h) {
+        if (isRed(h.left)) {
+            h = rotateRight(h);
+        }
+        if (h.right == null) {
+            return null;
+        }
+        /**
+         * 若根节点的父节点中有一个节点为空，另一个不为空，那么该不为空
+         * 的节点一定为红节点
+         */
+        if (!isRed(h.right) && !isRed(h.right.left)) {
+            h = moveRedRight(h);
+        }
+        h.right = deleteMax(h.right);
+        return balance(h);
+    }
+
+    private Node moveRedRight(Node h) {
+        flipColors(h);
+        if (isRed(h.left.left)) {
+            h = rotateRight(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    public void delete(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        if (!contains(key)) {
+            return;
+        }
+        if (!isRed(root.left) && isRed(root.right)) {
+            root.color = RED;
+        }
+        root = delete(root, key);
+        if (!isEmpty()) {
+            root.color = BLACK;
+        }
+    }
+
+    private Node delete(Node h, Key key) {
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left)) {
+                h = moveRedLeft(h);
+            }
+            h.left = delete(h.left, key);
+        } else {
+            if (isRed(h.left)) {
+                h = rotateRight(h);
+            }
+            /**
+             * 要删除的节点正好在底部，直接删除
+             */
+            if (key.compareTo(h.key) == 0 && h.right == null) {
+                return null;
+            }
+            if (!isRed(h.right) && !isRed(h.right.left)) {
+                h = moveRedRight(h);
+            }
+            if (key.compareTo(h.key) == 0) {
+                Node x = min(h.right);
+                h.key = x.key;
+                h.val = x.val;
+                h.right = deleteMin(h.right);
+            } else {
+                h.right = delete(h.right, key);
+            }
+        }
+        return balance(h);
+    }
+
+    public Key min() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("calls min() with empty symbol table");
+        }
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        } else {
+            return min(x.left);
+        }
+    }
+
+    public Key max() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("calls max() with empty symbol table");
+        }
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) {
+            return x;
+        } else {
+            return max(x.right);
+        }
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        if (lo == null) {
+            throw new IllegalArgumentException("first argument to keys() is null");
+        }
+        if (hi == null) {
+            throw new IllegalArgumentException("second argument to keys() is null");
+        }
+        Queue<Key> queue = new ArrayDeque<>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) {
+            return;
+        }
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) {
+            keys(x.left, queue, lo, hi);
+        }
+        if (cmplo <= 0 && cmphi >= 0) {
+            queue.add(x.key);
+        }
+        if (cmphi > 0) {
+            keys(x.right, queue, lo, hi);
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Key k : keys()) {
+            sb.append("[" + k + "," + get(k) + "]" + " ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        RedBlackBST<Character,Integer> redBlackBST=new RedBlackBST<>();
+        String str = "SEARCHEXAMPLE";
+        for (int i = 0; i < str.length(); i++) {
+            redBlackBST.put(str.charAt(i), i);
+        }
+        System.out.println(redBlackBST.toString());
+        redBlackBST.deleteMin();
+        System.out.println(redBlackBST.toString());
     }
 }
---
+++ src/nowcoder/select/Test.java
@@ -0,0 +1,48 @@
+package nowcoder.select;
+
+/**
+ * @author: Hello World
+ * @date: 2018/7/13 10:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new B().getValue());
+    }
+
+    static class A {
+        protected int value;
+
+        public A(int v) {
+            setValue(v);
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            try {
+                value++;
+                return value;
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            } finally {
+                this.setValue(value);
+                System.out.println(value);
+            }
+            return value;
+        }
+    }
+
+    static class B extends A {
+        public B() {
+            super(5);
+            setValue(getValue() - 3);
+        }
+
+        @Override
+        public void setValue(int value) {
+            super.setValue(2 * value);
+        }
+    }
+}
