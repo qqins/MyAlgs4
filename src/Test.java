3e16d857c5b97cbcfcb38a2f2a2868a28ad55d88
qqins <1007797840@qq.com>
2019-04-03 21:41:57 +0800

    add

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,6 @@
 public class Test {
-    private int[] array = new int[5];
-    private int N = 0;
-
-    public void push(int item) {
-        if (N == array.length) {
-            resize(2 * array.length);
-        }
-        array[N++] = item;
-    }
-
-    private void resize(int size) {
-        int[] temp = new int[size];
-        for (int i = 0; i < N; i++) {
-            temp[i] = array[i];
-        }
-        array = temp;
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(++a > 0 || ++a > 0 ? a++ : ++a);
     }
 }
